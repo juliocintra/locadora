ceb195b8161be18e57cd24af4fb7fed6d6e7ae16
Julio Cintra <julio.cesar@smn.com.br>
2018-03-01 15:25:07 -0300

    correções

--- Locadora/front/scripts/request.js
+++ Locadora/front/scripts/request.js
@@ -1,11 +1,11 @@
 function mostrarSnackbar(message) {
     var snackBar = document.getElementById("snackbar");
     snackBar.className = "show";
-    snackbar.innerHTML = message;
+    snackBar.innerHTML = message;
 
     setTimeout(function(){
         snackBar.className = snackBar.className.replace("show", "");
-        }, 3000);
+        }, 1500);
 }
 
 function login() {
@@ -14,8 +14,9 @@ function login() {
 
     var params = JSON.stringify({"username": usuario,"password": senha});
 
-    if (usuario == '' || senha == '')
+    if (usuario == '' || senha == '') {
         mostrarSnackbar('Preencha todos os campos!');
+    }
     else {
 
         var request = new XMLHttpRequest();
@@ -36,47 +37,6 @@ function login() {
             }
         }
     }
-
-    // var username = document.getElementById("username").value;
-    // var password = document.getElementById("password").value;
-    //
-    // if(username.value != '' && password.value != '')
-    // {
-    //     var ajax = null;
-    //     var params = null;
-    //
-    //     params = JSON.stringify({"username": username,"password": password});
-    //
-    //     ajax = new XMLHttpRequest();
-    //     ajax.open("POST", "https://watchlater.azurewebsites.net/api/login", true);
-    //     ajax.setRequestHeader("Content-type", "application/json");
-    //     ajax.send(params);
-    //
-    //     ajax.onreadystatechange = function() {
-    //         if (ajax.readyState == 4)
-    //         {
-    //             if(ajax.status == 200)
-    //             {
-    //                 var retornoAjax = JSON.parse(ajax.responseText);
-    //                 localStorage.setItem("id_usuario", retornoAjax);
-    //                 // mostrarSnackbar(1, 'Login efetuado!');
-    //                 setTimeout(
-    //                     function(){
-    //                         fadeOut("cardLogin", 0.8);
-    //                         location.href = "home.php";
-    //                     }, 1500);
-    //             }
-    //             else
-    //             {
-    //                 // mostrarSnackbar(3, 'Dados Inválidos!');
-    //             }
-    //         }
-    //     }
-    // }
-    // else
-    // {
-    //     // mostrarSnackbar(2, 'Preencher todos os campos!');
-    // }
 }
 
 function cadastrar() {
@@ -103,16 +63,15 @@ function cadastrar() {
         request.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 mostrarSnackbar('Cadastro efetuado com sucesso!!');
-                setTimeout(function() {
-                    voltar();
-                }, 3000)
-
             }
             else if(this.status == 400){
                 if (request.responseText) {
                     var message = JSON.parse(request.responseText);
                 }
-                mostrarSnackbar(message);
+                mostrarSnackbar(message[0]);
+                setTimeout(function() {
+                    voltar();
+                }, 1500)
             }
         };
     }
