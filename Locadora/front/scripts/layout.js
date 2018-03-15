fa580ad819265dfa1a14ce27250b7dab2e39649f
Julio Cintra <julio.cesar@smn.com.br>
2018-03-15 15:35:05 -0300

    css barra menu

--- Locadora/front/scripts/layout.js
+++ Locadora/front/scripts/layout.js
@@ -29,6 +29,18 @@ function transitionMenu() {
     if (menu === 'divVertical') {
         document.getElementById("divVertical").classList.remove("divVertical");
         document.getElementById("divVertical").classList.add("expandirDivVertical");
+
+        document.getElementById("barra1").classList.remove("barra1");
+        document.getElementById("barra1").classList.add("rotateBarra1");
+
+        document.getElementById("barra3").classList.remove("barra3");
+        document.getElementById("barra3").classList.add("rotateBarra3");
+
+        document.getElementById("barra2").classList.remove("barra2");
+        document.getElementById("barra2").classList.add("hiddenBarra2");
+
+        // document.getElementById("barra2").style.transform = "rotate(-45deg)"
+
         // document.getElementById("menu").classList.remove("menu");
         // document.getElementById("menu").classList.add("expandirMenu");
         //
@@ -39,16 +51,25 @@ function transitionMenu() {
         document.getElementById("divVertical").classList.remove("expandirDivVertical");
         document.getElementById("divVertical").classList.add("divVertical");
 
+        document.getElementById("barra1").classList.remove("rotateBarra1");
+        document.getElementById("barra1").classList.add("barra1");
+
+        document.getElementById("barra3").classList.remove("rotateBarra3");
+        document.getElementById("barra3").classList.add("barra3");
+
+        document.getElementById("barra2").classList.remove("hiddenBarra2");
+        document.getElementById("barra2").classList.add("barra2");
+
         // document.getElementById("menu").classList.remove("expandirMenu");
         // document.getElementById("menu").classList.add("menu");
         //
         // document.getElementById("longBarVertical").classList.remove("expandirLongBarVertical");
         // document.getElementById("longBarVertical").classList.add("longBarVertical");
     }
+
 }
 
 function miniPosterEditar(urlPoster) {
     document.getElementById("poster").style.backgroundImage = "url('"+urlPoster+"')";
     // document.getElementById("poster").style.backgroundColor = 'red';
-
 }
