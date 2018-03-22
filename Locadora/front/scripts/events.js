14f8accb74215d97509ec50201aea093454d64c2
Julio Cesar <julinn_cesar@hotmail.com>
2018-03-22 01:51:36 -0300

    teste

--- Locadora/front/scripts/events.js
+++ Locadora/front/scripts/events.js
@@ -12,6 +12,25 @@ function mostrarSnackbar(message, status) {
     }, 1500);
 }
 
+function darkTheme() {
+    var body = document.getElementById("body").className;
+
+    if (body === 'bodyDark'){
+        document.getElementsByTagName("BODY")[0].classList.remove("bodyDark");
+        document.getElementById("listaFilmes").style.backgroundColor = "rgb(221, 221, 221)";
+    }
+    else {
+        document.getElementsByTagName("BODY")[0].classList.add("bodyDark");
+        document.getElementById("listaFilmes").style.backgroundColor = "#2b2b2b";
+    }
+
+    var teste = document.getElementById("listaFilmes").childNodes;
+    teste.forEach(function(item) {
+        document.getElementById(item.id.toString()).style.backgroundColor = "red";
+    })
+
+}
+
 function validaUser(btn) {
    console.log(btn.value);
    // var regex = /^[a-z0-9_-]/;
